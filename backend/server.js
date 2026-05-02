b6277bf65cd7fc291c3ff8a7bd425b693c3fa1ee
Mudra Bhedi <bhedi959@gmail.com>
2026-05-02 16:35:38 +0530

    fix checkout deploy

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://trendyythreads.onrender.com",],
+    origin: ["http://localhost:5173","https://trendyythreads-1.onrender.com",],
     
     credentials: true,
   })
